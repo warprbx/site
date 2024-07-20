316a6ce793a1cce0d9f85775a507d825dde071f1
warp <163360653+warprbx@users.noreply.github.com>
2024-07-20 13:56:21 -0400

    Update night.lua

--- night.lua
+++ night.lua
@@ -1,3 +1,3 @@
-join the discord server for night here to get the script.
-https://discord.gg/linkvertise
-DONT EXECUTE 
+-- join the discord server for night here to get the script.
+-- https://discord.gg/linkvertise
+-- DONT EXECUTE 
